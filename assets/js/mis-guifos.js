8adf470c399b0dfd63d79761a080cad6c4b56bbd
paudiaz122 <pau.diaz@hotmail.com.ar>
2020-04-13 22:45:45 -0300

    upgrade: mis guifos almost ready

--- assets/js/mis-guifos.js
+++ assets/js/mis-guifos.js
@@ -3,8 +3,10 @@ function createGuifos() {
     //tomo todos los elementos de html que necesito mostrar u ocultar
     const navButton = document.getElementsByClassName("nav-button")[0];
     const createGuifosWindow = document.getElementsByClassName("create-guifos")[0];
+    const goBakcArrow = document.getElementsByClassName("goback")[0];
 
     //muestro u oculto los elementos
     navButton.classList.add("hidden");
     createGuifosWindow.classList.remove("hidden");
+    goBakcArrow.classList.remove("hidden");
 }
